727075a76e09b732dc4613b1c657c5ed2b4947bd
vlad.butsko <butko.wladislaw@gmail.com>
2018-02-20 14:39:48 +0300

    triangle resolved

--- src/index.js
+++ src/index.js
@@ -4,4 +4,23 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    if(preferences === null || !Array.isArray(preferences) || preferences.length < 3){
+        return 0;
+    }
+    let prefDistinct = [0].concat(preferences); // make index of person equal it's number --> code more clear
+
+    let triangleCount = 0;
+    prefDistinct.forEach(function(lovedPerson, personNumber, array) {
+        let lovedOfLovedPerson = array[lovedPerson];
+        if(lovedOfLovedPerson === personNumber){
+            return;
+        }
+        let lovedOfLovedOfLoved = array[lovedOfLovedPerson];
+        if(lovedOfLovedOfLoved === personNumber){
+            triangleCount++;
+        }
+    });
+
+    return triangleCount/3;
+
 };
